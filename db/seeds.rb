d4c3455b72852cd6022e4381ac70aa8f44af415b
Matthew Swan <swanm86@gmail.com>
2016-04-21 09:00:02 -0500

    new rails app for ToolShed app

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Tool.create(name: 'Hoe', price: 2.00, quantity: 7)
+Tool.create(name: 'Carpenter Square', price: 46.00, quantity: 2)
+Tool.create(name: 'Chalk Line', price: 0.50, quantity: 10)
+Tool.create(name: 'Tape Measure', price: 8.00, quantity: 7)
+
